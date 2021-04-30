0469e59ed4ea1aeeca91cc300e0450adccbeda65
Pavel Pichrt <pavel.pichrt@reachplc.com>
2021-04-30 11:57:39 +0100

    Clarify sorting requirements

--- src/04-sort-array/index.js
+++ src/04-sort-array/index.js
@@ -1,9 +1,9 @@
 /**
- * LEVEL 1: Implement using Array.prototype methods and NPM packages (e.g.: check-types)
+ * LEVEL 1: Implement using Array.prototype.sort method and NPM packages (e.g.: check-types)
  *   - REQUIRED
- * LEVEL 2: Implement without using Array.prototype methods or libraries without regard to efficiency
+ * LEVEL 2: Implement without using Array.prototype.sort method or libraries, without regard to efficiency
  *   - Not required, but bonus points awarded
- * LEVEL 3: Implement without using Array.prototype methods or libraries efficiently
+ * LEVEL 3: Implement without using Array.prototype.sort method or libraries, efficiently
  *   - Not required, but major bonus points awarded
  */
 
